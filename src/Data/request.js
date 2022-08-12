87be7e9d162f2ad01a1352938193eca0adfca559
Matt Logan <mlogan5212@gmail.com>
2022-08-11 23:27:06 -0500

    Get Movie Data, Build Rows

---
+++ src/Data/request.js
@@ -0,0 +1,19 @@
+import apiKeys from './apikeys.json';
+
+const tmdbKey = apiKeys.tmdbConfig.apiKey;
+
+console.log(tmdbKey)
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${tmdbKey}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${tmdbKey}&with_networks=213`,
+  fetchTopRated: `movie/top_rated?api_key=${tmdbKey}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${tmdbKey}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${tmdbKey}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${tmdbKey}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${tmdbKey}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${tmdbKey}&with_genres=99`,
+}
+
+
+export default requests;
